428b51985cd99a4389b0d3904567702ce5da0975
Vicked9 <rachurivs@gmail.com>
2020-01-19 18:53:20 +0530

    First creation of window and label

---
+++ GUIFirst.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+window = Tk() # Creates the window class
+
+window.title("First Window")
+
+# Creating a label class
+
+lbl = Label(window,text="Hello",\
+    font =("Arial Bold", 50))
+lbl.grid(row=0,column=0)
+
+# Setting window size 
+window.geometry('350x200')
+
+window.mainloop()# for the window to appear to users
+
+
